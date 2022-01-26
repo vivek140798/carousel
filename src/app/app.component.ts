caced506e37cc965998d5c678fe7bfcbf57c8a65
potula-vivek <vivek.p@convosight.com>
2022-01-26 08:59:17 +0530

    fc

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-carousel';
+  customOptions: OwlOptions = {
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      726: {
+        items: 2
+      },
+      1280: {
+        items: 3
+      }
+    },
+    nav: true
+  }
+  private reviews = [{
+    rating: 3,
+    review: 'My wife and I unequivocally recommend anyone interested in purchasing solar for their home to work through Electrum. Even if you are well informed about the options, your life will be made much easier',
+    name: 'JOHN FREY',
+    date: '2021-06-01'
+  },
+  {
+    rating: 4,
+    review: 'My wife and I unequivocally recommend anyone interested in purchasing solar for their home to work through Electrum. Even if you are well informed about the options, your life will be made much easier',
+    name: 'JOHN two',
+    date: '2021-06-01'
+  },
+  {
+    rating: 5,
+    review: 'My wife and I unequivocally recommend anyone interested in purchasing solar for their home to work through Electrum. Even if you are well informed about the options, your life will be made much easier',
+    name: 'JOHN three',
+    date: '2021-06-01'
+  },
+  {
+    rating: 5,
+    review: 'My wife and I unequivocally recommend anyone interested in purchasing solar for their home to work through Electrum. Even if you are well informed about the options, your life will be made much easier',
+    name: 'JOHN four',
+    date: '2021-06-01'
+  },
+  {
+    rating: 5,
+    review: 'My wife and I unequivocally recommend anyone interested in purchasing solar for their home to work through Electrum. Even if you are well informed about the options, your life will be made much easier',
+    name: 'JOHN five',
+    date: '2021-06-01'
+  },
+  {
+    rating: 5,
+    review: 'My wife and I unequivocally recommend anyone interested in purchasing solar for their home to work through Electrum. Even if you are well informed about the options, your life will be made much easier',
+    name: 'JOHN six',
+    date: '2021-06-01'
+  },
+  {
+    rating: 5,
+    review: 'My wife and I unequivocally recommend anyone interested in purchasing solar for their home to work through Electrum. Even if you are well informed about the options, your life will be made much easier',
+    name: 'JOHN seven',
+    date: '2021-06-01'
+  }];
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
